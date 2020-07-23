7affc3d1f313a44388b53229cd9fa32ec480b60e
ryo <sigaryoutakaki@gmail.com>
2020-07-23 16:41:14 +0900

    sql

--- 100pon_knock/51~60.sql
+++ 100pon_knock/51~60.sql
@@ -13,4 +13,33 @@ where customer_id not like 'Z%'
 group by customer_id
 limit 10
 
---53
+-- 53
+with
+    cust
+    as
+    (
+        select
+            c.customer_id ,
+            case when cast(substring(postal_cd, 0, 4) as integer ) >= 100 and
+                cast(substring(c.postal_cd, 0, 4) as integer ) <= 209 then 1 else 0 end as postal_flg,
+            c.postal_cd
+        from customer c
+    ),
+    rect
+    as
+    (
+        select
+            r.customer_id ,
+            sum(r.amount )
+        from receipt r
+        group by r.customer_id
+    )
+select
+    postal_flg
+	, count(*)
+from rect
+    inner join cust using(customer_id) 
+group by postal_flg3
+
+-- 54
+
---
+++ 100pon_knock/61~70.sql
@@ -0,0 +1,4 @@
+-- 63
+SELECT p.unit_price - p.unit_cost
+FROM product p
+LIMIT 10
